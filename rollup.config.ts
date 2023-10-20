9c1aa07d932a470c2edbd6039fc89b887fe0df34
gouan <394049183@qq.com>
2023-10-20 11:12:29 +0800

    feat:修改ts打包

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,16 +2,16 @@ import fs from "fs";
 import path from "path";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
-import dts from "rollup-plugin-dts";
 import { fileURLToPath } from "url";
-import { rollupDelete } from "./plugins/delete";
+import { rollupDelete } from "./src/plugins/delete";
 const __dirnameNew = path.dirname(fileURLToPath(import.meta.url));
 const packagesDir = path.resolve(__dirnameNew, "packages");
 const packageFiles = fs.readdirSync(packagesDir);
-function output(path: string) {
-  const prePath = `./packages/${path}`;
+function output(npath: string) {
+  const prePath = `./packages/${npath}`;
   const distPath = `${prePath}/dist`;
   const input = [`${prePath}/src/index.ts`];
+
   return [
     {
       input,
@@ -25,15 +25,17 @@ function output(path: string) {
           format: "cjs",
         },
       ],
-      plugins: [typescript({}), terser(), rollupDelete(`${distPath}`)],
-    },
-    {
-      input,
-      plugins: [dts()],
-      output: {
-        format: "esm",
-        file: `${prePath}/dist/index.d.ts`,
-      },
+      plugins: [
+        rollupDelete(`${distPath}`),
+        typescript({
+          declarationDir: `${distPath}/`,
+          declaration: true,
+          baseUrl: `${prePath}/`,
+          include: [`${prePath}/**/*.ts`],
+        }),
+
+        terser(),
+      ],
     },
   ];
 }
@@ -43,4 +45,4 @@ export default [
     .filter((item) => item != ".DS_Store")
     .map((path) => output(path))
     .flat(),
-];
+] as any[];
