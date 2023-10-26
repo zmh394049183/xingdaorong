aeeac35f0f63ed033b784c9f0dfb1a5d7cc002ba
gouan <394049183@qq.com>
2023-10-26 17:12:50 +0800

    feat:添加time

--- packages/time/src/index.ts
+++ packages/time/src/index.ts
@@ -1,4 +1,30 @@
-export * from "./b";
-export function add(a: number, b: number) {
-  return a + b;
+export function isYesterday(timestamp: number, now = new Date().getTime()) {
+  const yesterday = new Date(now - 24 * 60 * 60 * 1000);
+  const date = new Date(timestamp);
+  return (
+    date.getFullYear() === yesterday.getFullYear() &&
+    date.getMonth() === yesterday.getMonth() &&
+    date.getDate() === yesterday.getDate()
+  );
 }
+export const formatTimeView = (
+  timestamp: number,
+  now = new Date().getTime()
+) => {
+  const diff = now - timestamp;
+  const date = new Date(timestamp);
+  let getMinutes = (date.getMinutes() + "").padStart(2, "0");
+  let getHours = (date.getHours() + "").padStart(2, "0");
+  const timeStr = `${getHours}:${getMinutes}`;
+  if (isYesterday(timestamp, now)) {
+    return `昨天 ${timeStr}`;
+  } else if (diff < 60 * 1000) {
+    return "刚刚";
+  } else if (diff < 60 * 60 * 1000) {
+    return Math.floor(diff / (60 * 1000)) + "分钟前";
+  } else if (diff < 24 * 60 * 60 * 1000) {
+    return timeStr;
+  } else {
+    return date.getMonth() + 1 + "月" + date.getDate() + "日";
+  }
+};
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,31 @@
-export function sum(a: number, b: number) {
-  return a + b;
+function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null;
+  return function (...args: Parameters<T>): void {
+    const context = this;
+    const later = () => {
+      timeout = null;
+      func.apply(context, args);
+    };
+    timeout && clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
 }
+
+function throttle(func: Function, wait: number): () => void {
+  let pre: number = Date.now();
+
+  return function () {
+    const context = this;
+    const args = arguments;
+    const now = Date.now();
+
+    if (now - pre >= wait) {
+      func.apply(context, args);
+      pre = Date.now();
+    }
+  };
+}
+export { debounce, throttle };
